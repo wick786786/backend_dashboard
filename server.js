1733c5ee531fbf6d504421ee0b341c433d681713
Satyam Yadav <86571155+wick786786@users.noreply.github.com>
2024-01-16 16:58:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 const app = express();
 const port = 5000; // Set your desired port
 app.use(cors());
-app.use(cors({
-    origin: 'https://back-end-jlrx.onrender.com/', // Replace with your frontend's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}));
 
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 const pool = new Pool({
     user: 'satyamyadav',
     host: 'localhost',
